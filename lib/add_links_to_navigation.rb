26954a17681b845cd177fd627e23472ccb87646c
Samuel Giddins <segiddins@squareup.com>
2021-01-11 15:41:44 -0800

    Migrate to Middleman 4, CocoaPods 1.11.0.beta.2 and GH actions.

--- lib/add_links_to_navigation.rb
+++ lib/add_links_to_navigation.rb
@@ -6,20 +6,19 @@ require "nokogiri"
 # Our markdown parser gives us an id to each header ( which is cool ) but we also need an anchor in there for selection
 # which means we have to do it all ourselves.
 
-module AddLinksToNavigation
-  class << self
-
-    def registered(app, options={})
+class AddLinksToNavigation < Middleman::Extension
+  def initialize(app, options_hash={}, &block)
+      super
 
       app.after_render do |body, path, locs, template_class|
-        
+
         # we get multiple render calls due to markdown / slim doing their thing
-        # 
-        if (template_class.to_s.index "Slim") != nil or (path.to_s.index "templates") != nil     
-                            
+        #
+        if (template_class.to_s.index "Slim") != nil or (path.to_s.index "templates") != nil
+
           doc = Nokogiri::HTML(body)
           nodes = doc.css("#content-wrapper h2[id], #content-wrapper h3[id]")
-          
+
           if nodes.count > 0
             nodes.each do |header|
               if header.attributes["id"]
@@ -30,14 +29,12 @@ module AddLinksToNavigation
             end
 
             body = doc.to_s
-          end         
+          end
         end
-        
+
         body
       end
     end
-    alias :included :registered
-  end
 end
 
 ::Middleman::Extensions.register(:add_links_to_navigation) do
